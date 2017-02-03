2b3fd19604da9da7b0acef4c1d5605a3d06f7390
Emmanuel Levijarvi <emansl@gmail.com>
2017-02-03 13:58:35 -0800

    bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ for rst in ('README.rst', 'LICENSE.rst'):
 
 
 setup(name='censusacs',
-      version='0.9.3',
+      version='0.9.4',
       description=description,
       long_description='\n\n'.join(long_descriptions),
       author='Kevala Analytics, Inc.',
